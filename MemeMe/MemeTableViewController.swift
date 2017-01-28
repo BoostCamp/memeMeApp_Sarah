44063414240e5ed4335ed54a2b3b493db19ac5f6
sarahyoon <yoonsarah88@gmail.com>
2017-01-28 23:43:34 +0900

    edit1

--- MemeMe/MemeTableViewController.swift
+++ MemeMe/MemeTableViewController.swift
@@ -26,12 +26,10 @@ class MemeTableViewController: UITableViewController {
         
     }
     
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return memes.count
     }
 
-   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let meme = memes[indexPath.row]
