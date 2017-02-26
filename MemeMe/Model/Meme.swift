adba80e11297604abd3fb203e5db323355f0b149
Sarah Yoon <yoonsarah88@gmail.com>
2017-02-27 01:31:49 +0900

    Edit missing File -Meme

---
+++ MemeMe/Model/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Sarah Yoon on 2017. 2. 27..
+//  Copyright © 2017년 Udacity. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    var topText: String!
+    var bottomText: String!
+    var originalImage: UIImage!
+    var memedImage: UIImage!
+}
